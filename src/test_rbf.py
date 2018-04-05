4e7a60af46ee45cde039734ba16bc264e9e6e329
Jack Laurenson <jacklaurenson@gmail.com>
2018-04-06 07:32:45 +1200

    Normalising per cluster and then summing produces a predictable result. The biggest gradient occurs on the diagonal, i.e the biggest possible normalised gradient is the one at a 45 degree angle, so naturally the points all align themselves on the diagonal, at which point they get stuck, if I include the softmax part of the gradient, this could fix this

--- src/test_rbf.py
+++ src/test_rbf.py
@@ -8,9 +8,10 @@ g_new_grad = None
 def _normalised(unused_op, grad):
     global g_grad
     global g_new_grad
-    #m, d = grad.shape
+    m, d, K = grad.shape
+    K = K.value
     grad_mag = tf.reduce_sum(grad ** 2.0, axis=1) ** 0.5
-    new_grad = grad / tf.reshape(grad_mag, [-1, 1])
+    new_grad = grad / tf.reshape(grad_mag, [-1, 1, K])
     g_grad = grad
     g_new_grad = new_grad
     return new_grad
@@ -33,11 +34,11 @@ class RBF:
                                           initializer=tf.constant_initializer(np.array([[0.25, 0.25], [0.25, 0.25]]))))#initializer=tf.truncated_normal_initializer(stddev=0.5)))
         self.tau_square = tf.reshape(self.tau ** 2.0, [1, self.d, self.num_class])
         g = tf.get_default_graph()
-        with g.gradient_override_map({'Identity': "normalised"}):
-            z_identity = tf.identity(self.z, name='Identity')
-        z_re = tf.reshape(z_identity, [-1, self.d, 1])
+        z_re = tf.reshape(self.z, [-1, self.d, 1])
         z_tile = tf.tile(z_re, [1, 1, self.d])
-        x_diff = tf.subtract(z_tile, tf.reshape(self.z_bar, [1, self.d, self.num_class]), name='Sub')
+        with g.gradient_override_map({'Identity': "normalised"}):
+            z_identity = tf.identity(z_tile, name='Identity')
+        x_diff = tf.subtract(z_identity, tf.reshape(self.z_bar, [1, self.d, self.num_class]), name='Sub')
         self.x_diff_sq = x_diff ** 2.0
         self.weighted_x_diff_sq = tf.multiply(self.tau_square, self.x_diff_sq)
         self.neg_dist = -tf.reduce_sum(self.weighted_x_diff_sq, axis = 1)
