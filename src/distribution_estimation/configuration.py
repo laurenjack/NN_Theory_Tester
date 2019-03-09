Corrected sign of gradient checker

src/distribution_estimation/configuration.py
@@ -19,15 +19,15 @@ class Configuration:
 
     def __init__(self):
         # The number of observations in the dataset.
-        self.n = 20000
+        self.n = 2000
         # The number of examples for training at each step
         self.m = 1000
         # The number of reference examples (those part of the Kernel density estimate) for each training step
-        self.r = 10000
+        self.r = 1000
         # The number of dimensions, for the random variable a
         self.d = 1
         # The initial value of R
-        self.R_init = (0.4 / float(self.d)) * np.eye(self.d) # 0.5 * np.array([[1.0, 1.0 / 2.0 ** 0.5], [0.0, 1.0 / 2.0 ** 0.5]])
+        self.R_init = (0.51581488872 / float(self.d)) * np.eye(self.d) # 0.5 * np.array([[1.0, 1.0 / 2.0 ** 0.5], [0.0, 1.0 / 2.0 ** 0.5]])
         # [float] - A list of means, one for each Gaussian in the actual distribution
         self.means = np.zeros((1, self.d)) #  np.array([[-2.0, -2.0], [0.0, 2.0], [2.0, 2.0]])
         # The number of training epochs

src/distribution_estimation/gradient_experimenter.py
@@ -47,12 +47,14 @@ def show_gradient():
         a_square = a_difference ** 2.0
 
         front_weight = K * a_difference / fx
-        main_delta = a_square - np.mean(K * a_square, axis=1).reshape(s, 1) / fx
+        main_delta = np.mean(K * a_square, axis=1).reshape(s, 1) / fx - a_square
         complete_term = front_weight * main_delta
         reduced_over_a = np.mean(complete_term, axis=0)
         reduced_over_a_star = np.mean(reduced_over_a)
-        plt.scatter(a_star, 100000 * np.mean(front_weight, axis=0) * reduced_over_a)
-        # plt.scatter(a_star, reduced_over_a, color='r')
+        full_term_before_reduction = np.mean(front_weight, axis=0) * reduced_over_a
+        plt.scatter(a_star, np.mean(front_weight, axis=0))
+        plt.scatter(a_star, reduced_over_a, color='r')
+        plt.scatter(a_star, full_term_before_reduction, color='g')
         plt.show()
         print reduced_over_a_star
         total += reduced_over_a_star