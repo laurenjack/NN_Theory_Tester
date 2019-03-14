Low Bias high variance using fixed h works exceptionally well. For a single reference draw, as performed in the gradient_experimenter, the loss function is high variance from run to run. However, when taking a different draw of a_star2 for each epoch, h converges to the optimal h. Futhermore, at least by eyeball, there is no discernable difference in the variance of this and the vairance when moving towards known p(a). Will experiment with a dynamic estimate for p(a). This may not be required, but it would limit the need to set a specific low h, and also make training possible if there is a large difference between the variable h and its estimate.

src/distribution_estimation/gradient_experimenter.py
@@ -20,7 +20,7 @@ def show_variance_function():
     x, _ = data_generator.sample_gaussian_mixture()
 
     # Initialise the distribution fitter
-    kde = kernel_density_estimator.KernelDensityEstimator(conf, data_generator)
+    kde = kernel_density_estimator.KernelDensityEstimator(conf) #, data_generator)
 
     # Tensorflow setup
     session = tf.InteractiveSession()
@@ -29,7 +29,7 @@ def show_variance_function():
     # Create a placeholder to vary h
     A_inverse_placeholder = tf.placeholder(dtype=tf.float32, shape=[1, 1], name = 'A_inverse_placeholder')
     r = conf.r
-    s = conf.n - r
+    s = conf.n - 2*r
     loss_tensor = kde.loss(A_inverse_placeholder)
 
     hs = np.arange(0.05, 1.01, 0.01)
@@ -41,11 +41,12 @@ def show_variance_function():
         print h
         A_inverse = np.array([[1.0 / h]])
 
-        a_star = x[0:r]
-        a = x[r:]
+        a_star1 = x[0:r]
+        a_star2 = x[r:2*r]
+        a = x[2*r:]
 
-        loss = session.run(loss_tensor, feed_dict={kde.a: a, kde.a_star1: a_star, kde.batch_size: s,
-                                                            A_inverse_placeholder: A_inverse})
+        loss = session.run(loss_tensor, feed_dict={kde.a: a, kde.a_star1: a_star1, kde.a_star2: a_star2,
+                                                   kde.batch_size: s, A_inverse_placeholder: A_inverse})
         losses.append(loss)
 
     losses = np.array(losses)

src/distribution_estimation/kernel_density_estimator.py
@@ -17,6 +17,7 @@ class KernelDensityEstimator(object):
         self.batch_size = tf.placeholder(dtype=tf.int32, shape=[], name='batch_size')
         self.a = tf.placeholder(dtype=conf.float_precision, shape=[None, self.d], name='a')
         self.a_star1 = tf.placeholder(dtype=conf.float_precision, shape=[self.r, self.d], name='a_star1')
+        self.a_star2 = tf.placeholder(dtype=conf.float_precision, shape=[self.r, self.d], name='a_star2')
 
     def loss(self, A_inverse):
         """This function returns a tensor to be minimised with respect to A_inverse (the inverse bandwidth matrix of the
@@ -29,17 +30,17 @@ class KernelDensityEstimator(object):
         distribution of d dimensions. Compute the kernel density estimate compute the squared weighted mean error,
         for each reference observation in a_star. TODO(Jack) expand on this
         """
-        fa = self.pdf(A_inverse)
+        fa = self.pdf(A_inverse, self.a_star1)
         # If a data generator was passed in, use the actual distribution from the data generator:
         if self.data_generator:
            pa_estimate = self.data_generator.pdf(self.a, self.batch_size)
         # Otherwise we have a real problem where the distribution is unknown
         else:
-            pa_estimate = None
+            pa_estimate = self.pdf(np.array([[10.0]], dtype=np.float32), self.a_star2)
         loss = 0.5 * tf.reduce_mean((fa - pa_estimate) ** 2.0)
         return loss
 
-    def pdf(self, A_inverse):
+    def pdf(self, A_inverse, a_star):
         """Compute f(a) for the [batch_size, d] set of points a, using the [r, d] set of reference points and the
         inverse bandwitdth matrix A_inverse.
 
@@ -50,7 +51,7 @@ class KernelDensityEstimator(object):
             A [batch_size] tensor. The relative likelihood f(a) for each element of a.
         """
         H_inverse = tf.matmul(A_inverse, tf.transpose(A_inverse))
-        difference = tf.reshape(self.a, [self.batch_size, 1, self.d]) - tf.reshape(self.a_star1, [1, self.r, self.d])
+        difference = tf.reshape(self.a, [self.batch_size, 1, self.d]) - tf.reshape(a_star, [1, self.r, self.d])
         distance_squared = tf.reshape(tf.tensordot(difference, H_inverse, axes=[[2], [0]]),
                                       [self.batch_size, self.r, 1, self.d])
         distance_squared = tf.matmul(distance_squared, tf.reshape(difference, [self.batch_size, self.r, self.d, 1]))